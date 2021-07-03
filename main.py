0c914793a74c6dce37239003b1f60d92bb008fab
jordirios <jordirios92@gmail.com>
2021-07-03 18:13:40 +0200

    Building the web

---
+++ main.py
@@ -0,0 +1,9 @@
+from flask import Flask, request
+from flask import render_template
+from flask import redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
