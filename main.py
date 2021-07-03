3a5a33698f533f50eb92490f6f9216ed45214453
jordirios <jordirios92@gmail.com>
2021-07-03 19:30:38 +0200

    Creating the database

--- main.py
+++ main.py
@@ -3,7 +3,24 @@ from flask import render_template
 from flask import redirect
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='template')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+db.create_all()
+
+#Create the database to fill the columns with the information given by the user
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(100))
+    done = db.Column(db.Boolean)
+
+
+@app.route('/')
+def tasks_list():
+    tasks = Task.query.all()
+    return render_template('list.html', tasks=tasks)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
